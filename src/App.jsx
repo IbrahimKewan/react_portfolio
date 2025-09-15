89c1916c340b59ab55a2ff8deac8652f531232af
Ibrahim Kewan <ibrahimkewan0@gmail.com>
2025-09-15 22:16:19 +0200

    Add icon font and styles for improved UI elements
    - Introduced icomoon font with various formats for better compatibility.
    - Added CSS rules for icon classes to ensure consistent rendering across browsers.
    - Defined specific icons with their respective Unicode values for use in the application.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import { useState } from "react";
-
+import Header from "./components/1-header/header";
+import Main from "./components/3-main/main";
+import Hero from "./components/2-hero/hero";
+import Contact from "./components/4-contact/contact";
+import Footer from "./components/5-footer/footer";
 function App() {
-    return <></>;
+    return (
+        <div className="container">
+            <Header />
+            <div className="divider"></div>
+            <Hero />
+            <div className="divider"></div>
+            <Main />
+            <div className="divider"></div>
+            <Contact />
+            <div className="divider"></div>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
--- src/components/1-header/header.jsx
+++ src/components/1-header/header.jsx
@@ -1,7 +1,72 @@
-import React from "react";
+import { useState } from "react";
 import "./header.css";
 const Header = () => {
-    return <div>HEADER</div>;
+    const [showModal, setshowModal] = useState(false);
+    return (
+        <header className="flex">
+            <button
+                className="menu icon-menu flex"
+                onClick={() => {
+                    setshowModal(true);
+                }}
+            />
+            <div></div>
+            <nav>
+                <ul className="flex">
+                    <li>
+                        <a href="">About</a>
+                    </li>
+                    <li>
+                        <a href="">Project</a>
+                    </li>
+                    <li>
+                        <a href="">Contact</a>
+                    </li>
+                    <li>
+                        <a href="">test</a>
+                    </li>
+                    <li>
+                        <a href="">test</a>
+                    </li>
+                </ul>
+            </nav>
+
+            <button className="mode flex">
+                <span className="icon-moon-o"></span>
+                {/* <span className="icon-moon-o"></span> */}
+            </button>
+
+            {showModal && (
+                <div className="fixed">
+                    <ul className="modal">
+                        <li>
+                            <button
+                                className="icon-clear"
+                                onClick={() => {
+                                    setshowModal(false);
+                                }}
+                            />
+                        </li>
+                        <li>
+                            <a href="">About</a>
+                        </li>
+                        <li>
+                            <a href="">Project</a>
+                        </li>
+                        <li>
+                            <a href="">Contact</a>
+                        </li>
+                        <li>
+                            <a href="">test</a>
+                        </li>
+                        <li>
+                            <a href="">test</a>
+                        </li>
+                    </ul>
+                </div>
+            )}
+        </header>
+    );
 };
 
 export default Header;
